dd405ac132324a384da8fc072acb8be152669f8d
jackvsc <jackvalentineseacliff@gmail.com>
2020-09-10 21:34:11 -0400

    Update the Query File

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -8,3 +8,29 @@ INTO retirements
 FROM employees as e
 INNER JOIN titles as tl ON (e.emp_no = tl.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirements
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT title, COUNT(emp_no)
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tl.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS tl ON (e.emp_no = tl.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no ASC;
